80bd8cdd99803f50d54bfeb9d89ab4473a7e81b0
Harshit Pareek <hp1014@nyu.edu>
2021-01-17 22:10:58 -0800

    feat(adding the changes for new lessons on udemy)

--- app.js
+++ app.js
@@ -1,20 +1,68 @@
 const { app, BrowserWindow }  = require('electron');
+// const bcrypt = require('bcrypt');
+// const colors = require('colors');
 
+let win;
+// setTimeout(() => {
+//     console.log('Checking App is ready:' + app.isReady());
+// }, 2000);
+
+// console.log(colors.rainbow('Hello World!'));
+// bcrypt.hash('harshit', 10, (err, hash) => {
+//     debugger;
+//     console.log(hash);
+// });
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
+    win = new BrowserWindow({
+        width: 900,
         height: 600,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+        backgroundColor: 'white'
     });
 
     win.loadFile('index.html');
     win.webContents.openDevTools();
+
+    // win.once('ready-to-show', win.show);
+    // win.loadURL('https://google.com');
 }
 
-app.whenReady().then(createWindow);
+app.on('ready', () => {
+    // console.log('Home :', app.getPath('home'));
+    // console.log('Temp :', app.getPath('temp'));
+
+    // // Default location to store the data regarding the application
+    // console.log('UserData :', app.getPath('userData'));
+    // console.log('Desktop :', app.getPath('desktop'));
+    // console.log('Music :', app.getPath('music'));
+    app.setName('Electron Udemy');
+    console.log('Name of App: ', app.getName());
+    createWindow();
+});
+
+// Using instance method to quit the app after some time
+// app.on('browser-window-blur', () => {
+//     setTimeout(app.quit, 3000);
+// });
+
+// The Event hook for checking just before quitting
+// app.on('before-quit', (e) => {
+//     // preventing the default flow
+//     e.preventDefault();
+//     console.log('before quitting!');
+// });
+
+// Event to handle the focus and blur of the browser window
+// app.on('browser-window-blur', () => {
+//     console.log('window blurred');
+// });
+
+// app.on('browser-window-focus', () => {
+//     console.log('window focused');
+// });
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
---
+++ renderer.js
@@ -0,0 +1,3 @@
+// the file ran by the help of node integration in renderer process
+
+console.log(process);
