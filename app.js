0e8583697cd0ba91aed2841cb99ea40e8327d8aa
Harshit Pareek <hp1014@nyu.edu>
2021-01-25 17:11:30 -0800

    feat(adding the changes for the session)

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-const { app, BrowserWindow }  = require('electron');
+const { app, BrowserWindow, session }  = require('electron');
 
 
 let win, secWin;
 const createWindow = () => {
+    // let customSes = session.fromPartition('persist: part1');
+
+    // downloading the file
+    let ses = session.defaultSession;
+    
+    // Cookies
+    // let ses = session.defaultSession;
+
+    // const getCookies = () => {
+    //     ses.cookies.get({})
+    //         .then((cookie) => {
+    //             console.log(cookie);
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //         });
+    // }
+
     win = new BrowserWindow({
         width: 900,
         height: 600,
@@ -11,13 +29,68 @@ const createWindow = () => {
         },
     });
 
+    // secWin = new BrowserWindow({
+    //     width: 600,
+    //     height: 300,
+    //     webPreferences: {
+    //         nodeIntegration: true,
+    //         partition: 'persist: part1'
+    //         // session: customSes
+    //     },
+    // });
+
+    // let ses1 = win.webContents.session;
+    // let ses2 = secWin.webContents.session;
+    // let defaultSes = session.defaultSession;
+    // ses1.clearStorageData();
+
+    // console.log(Object.is(ses1, customSes));
+
     win.loadFile('index.html');
+    
+    ses.on('will-download', (e, downloadItem, webContents) => {
+        let filePath = downloadItem.getFilename();
+        let bytes = downloadItem.getTotalBytes();
+        
+        // Save to Desktop
+        downloadItem.setSavePath(app.getPath('desktop') + `/${filePath}`);
+
+
+        downloadItem.on('updated', (e, state) => {
+            let received = downloadItem.getReceivedBytes();
+
+            let progress = Math.floor((received/bytes) * 100);
+            
+            webContents.executeJavaScript(`window.progress.value = ${progress}`);
+        });
+    });
+    
+    // secWin.loadFile('index.html');
+    // win.loadURL('https://github.com');
+
+    // const myCookie = { url: 'https://myurl.com', name:'cookie1', value: 'some value', expirationDate: 1643155489 };
+
+    // ses.cookies.set(myCookie)
+    //     .then(() => {
+    //         console.log('custom cookie has been set');
+    //         getCookies();
+    //     });
+
+
+    // win.webContents.on('did-finish-load', () => {
+    //     getCookies();
+    // });
 
     win.on('closed', () => {
         win = null;
     });
 
+    // secWin.on('closed', () => {
+    //     secWin = null;
+    // });
+
     win.webContents.openDevTools();
+    // secWin.webContents.openDevTools();
 }
 
 app.on('ready', () => {
