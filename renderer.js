7dd57abf6654e02eec553e2f0bbb16c9722ce6ef
Harshit Pareek <hp1014@nyu.edu>
2021-01-27 11:26:40 -0800

    feat(adding changes for the renderer process)

--- renderer.js
+++ renderer.js
@@ -1,3 +1,14 @@
 // the file ran by the help of node integration in renderer process
 
-console.log(process);
+const { desktopCapturer } = require('electron');
+
+document.getElementById('screenshot-button').addEventListener('click', () => {
+    desktopCapturer.getSources({ types: ['window'], thumbnailSize: { width: 1440, height: 900 } })
+        .then((screens) => {
+            console.log(screens);
+            document.getElementById('screenshot-img').src = screens[0].thumbnail.toDataURL();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
