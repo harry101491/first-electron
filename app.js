d4d9662b45b19dc58b502cdb7d337b10c52ee6dc
Harshit Pareek <hp1014@nyu.edu>
2021-01-27 16:04:35 -0800

    feat(adding the ipc features)

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const { app, BrowserWindow }  = require('electron');
+const { app, BrowserWindow, ipcMain, dialog }  = require('electron');
 
 
 let win;
+
+const askFruit = async () => {
+    let fruits = [ 'Apple', 'Orange', 'Grape' ];
+
+    let choice = await dialog.showMessageBox({
+        message: 'Pick a Fruit',
+        buttons: fruits
+    });
+
+    return fruits[choice.response];
+};
+
 const createWindow = () => {
     win = new BrowserWindow({
         width: 900,
@@ -11,6 +23,12 @@ const createWindow = () => {
         },
     });
 
+    // setTimeout(() => {
+    //     askFruit().then((answer) => {
+    //         console.log(answer);
+    //     });
+    // }, 1000);
+
     win.loadFile('index.html');
 
     win.on('closed', () => {
@@ -18,7 +36,44 @@ const createWindow = () => {
     });
 
     win.webContents.openDevTools();
+
+    // win.webContents.on('did-finish-load', (e) => {
+    //     win.webContents.send('mailbox', {
+    //         recepient: 'harshit',
+    //         sender: 'main process',
+    //         data: {
+    //             value: 20
+    //         }
+    //     });
+    // });
 }
+
+// ipcMain.on('ask-fruit', (e, args) => {
+//     askFruit().then((answer) => {
+//         console.log(answer);
+//     });
+// });
+
+ipcMain.handle('ask-fruit', (e, args) => {
+    return askFruit();
+});
+
+
+// ipcMain.on('sync-message', (e, args) => {
+//     console.log(args);
+
+//     setTimeout(() => {
+//         e.returnValue = 'A Sync Response from the main process';
+//     }, 4000);
+// });
+
+
+// ipcMain.on('channel1', (e, args) => {
+//     console.log(args);
+
+//     // acknowleging or sending something back
+//     e.sender.send('channel1-response', 'Message received on channel1. Thank you!');
+// });
 
 app.on('ready', () => {
     createWindow();
--- renderer.js
+++ renderer.js
@@ -1,3 +1,38 @@
 // the file ran by the help of node integration in renderer process
 
-console.log(process);
+// Ask a Fruit Dialog
+const { ipcRenderer } = require('electron');
+
+
+document.getElementById('askFruit').addEventListener('click', (e) => {
+    // ipcRenderer.send('ask-fruit');
+    ipcRenderer.invoke('ask-fruit')
+        .then((answer) => {
+            console.log(answer);
+        });
+});
+
+
+
+// const { ipcRenderer } = require('electron');
+
+// // just to show the Renderer Thread will be blocked untill the sync job will not be done
+// let i = 0;
+// setInterval(() => {
+//     console.log(i);
+//     i++;
+// }, 1000);
+
+// document.getElementById('talk').addEventListener('click', (e) => {
+//     // ipcRenderer.send('channel1', 'Hello from the Renderer');
+//     let res = ipcRenderer.sendSync('sync-message', 'A sync message from renderer');
+//     console.log(res);
+// });
+
+// ipcRenderer.on('channel1-response', (e, args) => {
+//     console.log(args);
+// });
+
+// ipcRenderer.on('mailbox', (e, args) => {
+//     console.log(args);
+// });
